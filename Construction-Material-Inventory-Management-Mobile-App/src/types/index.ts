2d291d341ab6c60097de0e76f2305fed4d75b531
derekli5 <derekli153@gmail.com>
2026-04-02 13:50:09 -0400

    packages installed, src added

---
+++ Construction-Material-Inventory-Management-Mobile-App/src/types/index.ts
@@ -0,0 +1,49 @@
+export type  UserRole = 'Admin' |
+                        'Foreman' |
+                        'Logistics' |
+                        'ProjectManager';
+
+export interface Credential {
+    email: string;
+    hashedPassword: string;          // backend only
+    role: UserRole;
+    // permissionLevel: number; (omit)
+}
+
+export interface Material{
+    materialName: string
+    materialDescription: string;
+    materialAmount: number;
+}
+
+export interface Equipment{
+    equipmentName: string;
+    equipmentDescription: string;
+    equipmentSerialNumber: string;
+    equipmentAmount: number;
+}
+
+export interface Tool{
+    toolName: string;
+    toolIdNumber: string;
+}
+export interface Inventory{
+    materials: Material[];
+    equipment: Equipment[];
+    tools: Tool[];
+}
+
+export interface Jobsite{
+    jobsiteId: string;              // unique identifier for jobsite
+    jobsiteName: string;            // display name of jobsite
+    jobsiteAddress: string;         // street address of the jobsite
+    inventory: Inventory;
+}
+
+export interface Warehouse{
+    warehouseName: string;
+    warehouseAddress: string;
+    warehouseId: string;
+    deliveries: Delivery[];         // deliveries currently staged at this warehouse
+}
+
